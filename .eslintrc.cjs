a125e034a9eaf2906f95f0ef593ddb6405d0fcaf
MetaDow <9354693215@qq.com>
2023-03-02 10:10:13 +0800

    feat(init): 提交修复

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,31 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true
+    // 解决eslint找不到module的报错
+    // node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'standard-with-typescript', 'prettier', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   overrides: [],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json']
+    // project: ['./tsconfig.json'],
+    jsxPragma: 'React'
   },
-  plugins: ['vue'],
-  rules: {}
+  // plugins: ['vue'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
 }
